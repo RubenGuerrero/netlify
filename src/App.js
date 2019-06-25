33effcce9b50a8a23efd8510bd1ec7c5510cb43c
Rubén Guerrero <yo@rubenguerrero.com>
2019-06-25 20:28:28 +0200

    Change config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Yeah
-          { console.log(process.env) }
+          { console.log('Env config', process.env) }
         </p>
         <a
           className="App-link"
